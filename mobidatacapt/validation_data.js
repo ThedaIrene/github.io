41445a735d380e6b268224556e3732377faba536
ThedaIrene <theda@eijkman.go.id>
2017-07-14 16:02:22 +0700

    Add files via upload

---
+++ mobidatacapt/validation_data.js
@@ -0,0 +1,3 @@
+ var data_validation = '{"item2_text_1":{"required":true},"item6_text_1":{},"item8_select_1":{"required":true},"item10_select_1":{"required":true},"item11_select_1":{},"item12_select_1":{},"item14_text_1":{"required":true},"item15_text_1":{},"item19_text_1":{},"item20_text_1":{},"item22_text_1":{"required":true},"item23_text_1":{},"item24_text_1":{"required":true},"item27_text_1":{},"item28_text_1":{},"item30_text_1":{},"item31_text_1":{},"item32_select_1":{},"item33_textarea_1":{"maxlength":"10000"},"item38_tel_1":{"phoneUS":true}}';
+ var data_jsplugins = '[]';
+ var data_cssplugins = '[]';
